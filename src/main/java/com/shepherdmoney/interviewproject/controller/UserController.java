5d4ba42d9743bd38adf46758eec276cbfad0421e
Oscar Chen <oscar1149412@gmail.com>
2023-08-24 22:56:24 -0400

    cleaned up user controller methods

--- src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
@@ -9,11 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
-
+/*
+* I decided to return Response entities instead of POJOs
+* to maintain more control over HTTP requests in case we want to
+* provide additional metadata in the future.
+*/
 @RestController
 public class UserController {
 
-    // TODO: wire in the user repository (~ 1 line)
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -26,29 +29,19 @@ public class UserController {
         newUser.setName(payload.getName());
         newUser.setEmail(payload.getEmail());
         User createdUser = userService.createUser(newUser);
-
         return ResponseEntity.ok(createdUser.getId());
     }
 
     @DeleteMapping("/user/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable int id) {
         userService.deleteUser(id);
-        try {
-            userService.deleteUser(id);
-            return ResponseEntity.ok().build();
-        } catch (Exception ex) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                    "User with id {" + id + " } not found.",
-                    ex);
-        }
+        //service layer tries and catches exception
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/user/{id}")
     public ResponseEntity<User> getUser(@PathVariable int id) {
-        try {
-            return new ResponseEntity<>(userService.getUser(id), HttpStatus.OK);
-        } catch (UserNotFoundException ex) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
+        userService.getUser(id);
+        return ResponseEntity.ok().build();
     }
 }
